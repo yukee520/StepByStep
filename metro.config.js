8b0ab01dda0add0879c4492af30ac659efcfede4
yukee520 <yukee92tmc@hotmail.com>
2026-09-23 01:34:55 +0800

    fix(metro): wrap config with withNativeWind to resolve global.css

--- metro.config.js
+++ metro.config.js
@@ -1,15 +1,16 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { withNativeWind } = require('nativewind/metro');
 const path = require('path');
 
 const defaultConfig = getDefaultConfig(__dirname);
 
-const config = {
+const config = mergeConfig(defaultConfig, {
   resolver: {
     extraNodeModules: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
   watchFolders: [path.resolve(__dirname, 'src')],
-};
+});
 
-module.exports = mergeConfig(defaultConfig, config);
+module.exports = withNativeWind(config, { input: './global.css' });
